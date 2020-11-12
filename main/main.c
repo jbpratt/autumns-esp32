93f9761def74f658758b7e5633d15160c24eaa48
jbpratt78 <jbpratt78@gmail.com>
2020-11-12 17:09:43 -0600

    pull bme680 component from gschorcht/bme680-esp-idf

--- main/main.c
+++ main/main.c
@@ -35,12 +35,28 @@
 static const char *TAG = "APP";
 static const char *WIFI_TAG = "WIFI";
 static const char *WS_TAG = "WEBSOCKET";
-static const char *BME280_TAG = "BME280";
+
+/*
+echo "" | openssl s_client -showcerts -connect strims.gg:443 | sed -n \
+ "1,/Root/d; /BEGIN/,/END/p" | openssl x509 -outform PEM >certs/ca_cert.pem
+*/
+// strims ssl cert
+extern const uint8_t ws_cert_pem_start[] asm("_binary_ca_cert_pem_start");
+extern const uint8_t ws_cert_pem_end[] asm("_binary_ca_cert_pem_end");
+// Websocket signaling
+static TimerHandle_t shutdown_signal_timer;
+static SemaphoreHandle_t shutdown_sema;
+
+#ifdef CONFIG_ENABLED_BME680_SENSOR
+static const char *BME680_TAG = "BME680";
+#endif
+
 // static const char *HTTP_TAG = "HTTP";
 
 static EventGroupHandle_t s_wifi_event_group;
 
-#if CONFIG_ENABLE_BME280_SENSOR
+#ifdef CONFIG_ENABLE_BME280_SENSOR
+static const char *BME280_TAG = "BME280";
 
 static i2c_bus_handle_t i2c_bus = NULL;
 static bme280_handle_t dev = NULL;
@@ -52,19 +68,33 @@ static bme280_handle_t dev = NULL;
 #define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master do not need buffer */
 #define I2C_MASTER_FREQ_HZ 100000   /*!< I2C master clock frequency */
 
+static void i2c_bus_init(void) {
+  i2c_config_t conf;
+  conf.mode = I2C_MODE_MASTER;
+  conf.sda_io_num = I2C_MASTER_SDA_IO;
+  conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+  conf.scl_io_num = I2C_MASTER_SCL_IO;
+  conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+  conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+  i2c_bus = iot_i2c_bus_create(I2C_MASTER_NUM, &conf);
+}
+
+void bme280_run(void *pvParameters) {
+  ESP_LOGI("BME280:", "temperature:%f\n", iot_bme280_read_temperature(dev));
+  vTaskDelay(300 / portTICK_RATE_MS);
+  ESP_LOGI("BME280:", "humidity:%f\n", iot_bme280_read_humidity(dev));
+  vTaskDelay(300 / portTICK_RATE_MS);
+  ESP_LOGI("BME280:", "pressure:%f\n", iot_bme280_read_pressure(dev));
+  vTaskDelay(300 / portTICK_RATE_MS);
+}
+
+void bme280_init(void) {
+  i2c_bus_init();
+  dev = iot_bme280_create(i2c_bus, BME280_I2C_ADDRESS_DEFAULT);
+  ESP_LOGI(BME280_TAG, "iot_bme280_init:%d", iot_bme280_init(dev));
+  xTaskCreate(bme280_run, "bme280_run", 1024 * 2, NULL, 10, NULL);
+}
 #endif
-
-/*
-echo "" | openssl s_client -showcerts -connect strims.gg:443 | sed -n \
- "1,/Root/d; /BEGIN/,/END/p" | openssl x509 -outform PEM >certs/ca_cert.pem
-*/
-// strims ssl cert
-extern const uint8_t ws_cert_pem_start[] asm("_binary_ca_cert_pem_start");
-extern const uint8_t ws_cert_pem_end[] asm("_binary_ca_cert_pem_end");
-
-// Websocket signaling
-static TimerHandle_t shutdown_signal_timer;
-static SemaphoreHandle_t shutdown_sema;
 
 static void wifi_event_handler(void *arg, esp_event_base_t event_base,
                                int32_t event_id, void *event_data) {
@@ -218,33 +248,6 @@ void init_ws(void) {
   esp_websocket_client_destroy(client);
 }
 
-static void i2c_bus_init(void) {
-  i2c_config_t conf;
-  conf.mode = I2C_MODE_MASTER;
-  conf.sda_io_num = I2C_MASTER_SDA_IO;
-  conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-  conf.scl_io_num = I2C_MASTER_SCL_IO;
-  conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-  conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
-  i2c_bus = iot_i2c_bus_create(I2C_MASTER_NUM, &conf);
-}
-
-void bme280_run(void *pvParameters) {
-  ESP_LOGI("BME280:", "temperature:%f\n", iot_bme280_read_temperature(dev));
-  vTaskDelay(300 / portTICK_RATE_MS);
-  ESP_LOGI("BME280:", "humidity:%f\n", iot_bme280_read_humidity(dev));
-  vTaskDelay(300 / portTICK_RATE_MS);
-  ESP_LOGI("BME280:", "pressure:%f\n", iot_bme280_read_pressure(dev));
-  vTaskDelay(300 / portTICK_RATE_MS);
-}
-
-void bme280_init(void) {
-  i2c_bus_init();
-  dev = iot_bme280_create(i2c_bus, BME280_I2C_ADDRESS_DEFAULT);
-  ESP_LOGI(BME280_TAG, "iot_bme280_init:%d", iot_bme280_init(dev));
-  xTaskCreate(bme280_run, "bme280_run", 1024 * 2, NULL, 10, NULL);
-}
-
 void app_main(void) {
   ESP_LOGI(TAG, "Startup..");
   ESP_LOGI(TAG, "Free memory: %d bytes", esp_get_free_heap_size());
@@ -257,5 +260,9 @@ void app_main(void) {
   init_ws();
 #if CONFIG_ENABLE_BME280_SENSOR
   bme280_init();
+#endif
+
+#ifdef CONFIG_ENABLE_BME680_SENSOR
+//
 #endif
 }
