eccac43fe8e0217e42fc2c31615ece232b81fc41
jbpratt78 <jbpratt78@gmail.com>
2020-11-11 20:11:17 -0600

    pull in bme280 and i2c_bus from esp-iot-solution@release/v1.1

--- main/main.c
+++ main/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 #include "cJSON.h"
+#include "driver/i2c.h"
 #include "esp_event.h"
+#include "esp_http_client.h"
 #include "esp_log.h"
 #include "esp_spi_flash.h"
 #include "esp_system.h"
@@ -11,6 +13,8 @@
 #include "freertos/event_groups.h"
 #include "freertos/semphr.h"
 #include "freertos/task.h"
+#include "iot_bme280.h"
+#include "iot_i2c_bus.h"
 #include "nvs_flash.h"
 #include "sdkconfig.h"
 
@@ -21,24 +25,46 @@
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT BIT1
 
+// Websocket timeout if no data is received
 #define NO_DATA_TIMEOUT_SEC 60
 
-static EventGroupHandle_t s_wifi_event_group;
+// HTTP settings
+#define MAX_HTTP_RECV_BUFFER 512
+#define MAX_HTTP_OUTPUT_BUFFER 2048
+
 static const char *TAG = "APP";
 static const char *WIFI_TAG = "WIFI";
 static const char *WS_TAG = "WEBSOCKET";
+static const char *BME280_TAG = "BME280";
+// static const char *HTTP_TAG = "HTTP";
 
-extern const uint8_t server_cert_pem_start[] asm("_binary_ca_cert_pem_start");
-extern const uint8_t server_cert_pem_end[] asm("_binary_ca_cert_pem_end");
+static EventGroupHandle_t s_wifi_event_group;
 
+#if CONFIG_ENABLE_BME280_SENSOR
+
+static i2c_bus_handle_t i2c_bus = NULL;
+static bme280_handle_t dev = NULL;
+
+#define I2C_MASTER_SCL_IO 21        /*!< gpio number for I2C master clock IO21*/
+#define I2C_MASTER_SDA_IO 15        /*!< gpio number for I2C master data  IO15*/
+#define I2C_MASTER_NUM I2C_NUM_1    /*!< I2C port number for master dev */
+#define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master do not need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master do not need buffer */
+#define I2C_MASTER_FREQ_HZ 100000   /*!< I2C master clock frequency */
+
+#endif
+
+/*
+echo "" | openssl s_client -showcerts -connect strims.gg:443 | sed -n \
+ "1,/Root/d; /BEGIN/,/END/p" | openssl x509 -outform PEM >certs/ca_cert.pem
+*/
+// strims ssl cert
+extern const uint8_t ws_cert_pem_start[] asm("_binary_ca_cert_pem_start");
+extern const uint8_t ws_cert_pem_end[] asm("_binary_ca_cert_pem_end");
+
+// Websocket signaling
 static TimerHandle_t shutdown_signal_timer;
 static SemaphoreHandle_t shutdown_sema;
-
-static void shutdown_signaler(TimerHandle_t xTimer) {
-  ESP_LOGI(WS_TAG, "No data received for %d seconds, signaling shutdown",
-           NO_DATA_TIMEOUT_SEC);
-  xSemaphoreGive(shutdown_sema);
-}
 
 static void wifi_event_handler(void *arg, esp_event_base_t event_base,
                                int32_t event_id, void *event_data) {
@@ -105,6 +131,12 @@ void init_wifi(void) {
   vEventGroupDelete(s_wifi_event_group);
 }
 
+static void ws_shutdown_signaler(TimerHandle_t xTimer) {
+  ESP_LOGI(WS_TAG, "No data received for %d seconds, signaling shutdown",
+           NO_DATA_TIMEOUT_SEC);
+  xSemaphoreGive(shutdown_sema);
+}
+
 static void websocket_event_handler(void *handler_args, esp_event_base_t base,
                                     int32_t event_id, void *event_data) {
   esp_websocket_event_data_t *data = (esp_websocket_event_data_t *)event_data;
@@ -161,13 +193,13 @@ static void websocket_event_handler(void *handler_args, esp_event_base_t base,
 void init_ws(void) {
   const esp_websocket_client_config_t websocket_cfg = {
       .uri = CONFIG_WEBSOCKET_URI,
-      .cert_pem = (const char *)server_cert_pem_start,
+      .cert_pem = (const char *)ws_cert_pem_start,
   };
 
   shutdown_signal_timer =
       xTimerCreate("Websocket shutdown timer",
                    NO_DATA_TIMEOUT_SEC * 1000 / portTICK_PERIOD_MS, pdFALSE,
-                   NULL, shutdown_signaler);
+                   NULL, ws_shutdown_signaler);
   shutdown_sema = xSemaphoreCreateBinary();
 
   esp_websocket_client_handle_t client =
@@ -186,6 +218,33 @@ void init_ws(void) {
   esp_websocket_client_destroy(client);
 }
 
+static void i2c_bus_init(void) {
+  i2c_config_t conf;
+  conf.mode = I2C_MODE_MASTER;
+  conf.sda_io_num = I2C_MASTER_SDA_IO;
+  conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+  conf.scl_io_num = I2C_MASTER_SCL_IO;
+  conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+  conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+  i2c_bus = iot_i2c_bus_create(I2C_MASTER_NUM, &conf);
+}
+
+void bme280_run(void *pvParameters) {
+  ESP_LOGI("BME280:", "temperature:%f\n", iot_bme280_read_temperature(dev));
+  vTaskDelay(300 / portTICK_RATE_MS);
+  ESP_LOGI("BME280:", "humidity:%f\n", iot_bme280_read_humidity(dev));
+  vTaskDelay(300 / portTICK_RATE_MS);
+  ESP_LOGI("BME280:", "pressure:%f\n", iot_bme280_read_pressure(dev));
+  vTaskDelay(300 / portTICK_RATE_MS);
+}
+
+void bme280_init(void) {
+  i2c_bus_init();
+  dev = iot_bme280_create(i2c_bus, BME280_I2C_ADDRESS_DEFAULT);
+  ESP_LOGI(BME280_TAG, "iot_bme280_init:%d", iot_bme280_init(dev));
+  xTaskCreate(bme280_run, "bme280_run", 1024 * 2, NULL, 10, NULL);
+}
+
 void app_main(void) {
   ESP_LOGI(TAG, "Startup..");
   ESP_LOGI(TAG, "Free memory: %d bytes", esp_get_free_heap_size());
@@ -196,4 +255,7 @@ void app_main(void) {
   init_wifi();
 
   init_ws();
+#if CONFIG_ENABLE_BME280_SENSOR
+  bme280_init();
+#endif
 }
